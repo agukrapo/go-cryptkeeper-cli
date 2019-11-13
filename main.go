093927522065ab9b310eb3e037ad55905bf064c4
Agustin Krapovickas <agukrapo@gmail.com>
2019-11-13 11:15:48 -0300

    First working version

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"os"
+
+	"github.com/blaskovicz/go-cryptkeeper"
+)
+
+var (
+	operation string
+	key       string
+	value     string
+)
+
+func init() {
+	flag.StringVar(&operation, "o", "", "operation: encrypt or decrypt")
+	flag.StringVar(&key, "k", "", "encryption key")
+	flag.StringVar(&value, "v", "", "target value of the operation")
+
+	flag.Parse()
+}
+
+func main() {
+	if operation == "" || key == "" || value == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	if err := cryptkeeper.SetCryptKey([]byte(key)); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	var result string
+	var err error
+	switch operation {
+	case "encrypt":
+		result, err = cryptkeeper.Encrypt(value)
+	case "decrypt":
+		result, err = cryptkeeper.Decrypt(value)
+	default:
+		fmt.Println("Invalid opeation: ", operation)
+		os.Exit(1)
+	}
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(result)
+}
